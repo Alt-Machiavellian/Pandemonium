0cb070d2fc304e1fbf40c2c02abdfbea9f17f24b
Brandon <bbullock121@gmail.com>
2026-07-25 16:53:24 -0400

    lottery number project added

---
+++ Projects/Codecademy/LotteryNumberGenerator.ps1
@@ -0,0 +1,30 @@
+Function LotteryDraw{
+    param ($min, $max, $quantity)
+    for($i=0;$i -lt $quantity;$i++){
+        Get-Random -Minimum $min -Maximum $max
+
+    }
+}
+
+Function LotteryDraw{
+  param ($min = 1, $max = 50, $quantity = 5)
+  for($i = 0; $i -lt $quantity; $i++) {
+    Get-Random -Minimum $min -Maximum $max
+    Write-Host "Lottery Generator"
+  }
+}
+
+Function MatchThree{
+  Write-Host "Welcome to Match Three"
+  LotteryDraw -min 0 -max 10 -quantity 3
+}
+
+Function MegaLotto {
+  Write-Host "Welcome to Mega Lotto"
+  LotteryDraw -min 0 -max 50 -quantity 1
+}
+
+Function MagicBall {
+  Write-Host "Welcome to Magic Ball"
+  LotteryDraw -min 10 -max 75 -quantity 6
+}
