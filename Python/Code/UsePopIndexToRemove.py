b38ac0d2046da339cfbbece7bd6189e56cf51d3b
Brandon B. <56424124+Alt-Machiavellian@users.noreply.github.com>
2026-08-22 19:38:53 -0400

    Added two pratice files.

---
+++ Python/Code/UsePopIndexToRemove.py
@@ -0,0 +1,6 @@
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+
+data_science_topics.pop(5)
+data_science_topics.pop(3)
+
+print(data_science_topics)
