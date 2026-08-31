3aed758066955a59c97e3f88e334d02354e3a9ad
Brandon B. <56424124+Alt-Machiavellian@users.noreply.github.com>
2026-08-30 23:12:01 -0400

    Added ForLoop files

---
+++ Python/Code/WhileLoops( Introduction).py
@@ -0,0 +1,23 @@
+# While Loop Walkthrough
+# count = 0
+# print("Starting While Loop")
+# while count <= 3:
+#   # Loop Body
+#   # Print if the condition is still true
+#   print("Loop Iteration - count <= 3 is still true")
+#   # Print the current value of count 
+#   print("Count is currently " + str(count))
+#   # Increment count
+#   count += 1
+#   print(" ----- ")
+# print("While Loop ended")
+
+# Your code below:
+
+countdown = 10
+while countdown >= 10:
+  print("We have liftoff")
+  countdown += 1
+
+while countdown >= 0:
+  print()
